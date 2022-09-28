87de4aab9e235178fde66243cd0f6d40d5ed04f7
nnnidooo <nadi3252@stud.kea.dk>
2022-09-28 10:54:17 +0200

    made some questions + radio optioned buttons

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-function generateQuiz (questions,quizContainer, submitButton) {
-    function showQuestions (questions, quizContainer) {
-        //my code here
+
+
+const myQuestions = [
+    {
+        question: 'What is 5+10?',
+        answers: {
+            a: '3',
+            b: '15',
+            c: '7'
+    },
+    correctAnswer: 'b'},
+    {
+        question: 'What is 15/3?',
+        answers: {
+            a:'5',
+            b: '9',
+            c: '2'
+        },
+        correctAnswer: 'a'},
+    {
+        question: 'What is 5*5?',
+        answers: {
+            a:'25',
+            b: '20',
+            c: '31'
+        },
+        correctAnswer:'a'
     }
-    function showResult (questions, quizContainer, resultContainer) {
 
-    }
 
-}
-
+]
 
