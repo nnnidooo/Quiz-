59a1cac72e528c4d231a34508a1917c346f76f28
nnnidooo <nadi3252@stud.kea.dk>
2022-10-03 17:25:12 +0200

    Made result box with the answers

--- main.js
+++ main.js
@@ -3,17 +3,19 @@
 const finish = document.querySelector('#submitBtn');
 finish.addEventListener("click", function (){
     const correctAmount = document.querySelectorAll('.correct');
-    let howManyCorrect = 0;
+    let howManyCorrect =  0;
     for (let i = 0; i < correctAmount.length ; i++) {
         if (correctAmount[i].checked){
             howManyCorrect++;
         }
     }
-    console.log("You answered " + howManyCorrect + " correct");
+    document.getElementById('output').innerHTML = "You answered " + howManyCorrect + " correct"
 
 })
 //https://www.geeksforgeeks.org/hide-or-show-elements-in-html-using-display-property/ inspiration;
 
 document.getElementById('submitBtn').onclick = function () {
         document.getElementById('result').style.display = 'block';
+
 }
+
